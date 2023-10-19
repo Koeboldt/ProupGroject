7695c781e17399a01a3e98bf146bc302776b2136
Alex M <alexmajors0613@gmail.com>
2023-10-19 09:02:57 -0500

    add recipe.html and change view recipe buttons to redirect to it

--- assets/js/scriptMealDB.js
+++ assets/js/scriptMealDB.js
@@ -1,17 +1,3 @@
-/* var broccoli = "747447";
-var url =
-  "https://api.nal.usda.gov/fdc/v1/food/" +
-  broccoli +
-  "%20?format=full&nutrients=203&nutrients=204&nutrients=205&api_key=CgqMH6hMOSehsdQ4BUkItOfNsoU7eCCHr4D8YL1h";
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("data :", data);
-    console.log("total protein :", data.foodNutrients[0].amount);
-    console.log("total fats :", data.foodNutrients[1].amount);
-    console.log("Carbohydrates :", data.foodNutrients[2].amount);
-  }); */
-
 /* fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
   .then(function (responce) {
     return responce.json();
@@ -76,41 +62,6 @@ fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=chicken")
       });
   });
 
-/* function getInformation(ingredients) {
-  for (i = 0; i < ingredients.length; i++) {
-    let ingredient = ingredients[i].replace(" ", "-");
-    fetch(
-      `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${keys.fdcKey2}&query=${ingredient}`
-    )
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          console.log(
-            "Error URL:",
-            `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${keys.fdcKey2}&query=${ingredient}`
-          );
-        }
-      })
-      .then((data) => {
-        console.log(
-          "URL:",
-          `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${keys.fdcKey2}&query=${ingredient}`
-        );
-        const food = data.foods[0];
-        let nutrients = food.foodNutrients;
-        var nutrientsObj = {};
-        console.log("Ingrident:", food.ingredients);
-        console.log("Serving Size:", food.servingSize + food.servingSizeUnit);
-        for (i = 0; i < nutrients.length; i++) {
-          nutrientsObj[nutrients[i].nutrientName] =
-            nutrients[i].nutrientNumber + nutrients[i].unitName.toLowerCase();
-        }
-        console.log("Nutrients:", nutrientsObj);
-      });
-  }
-} */
-
 function saveRecipe(event) {
   event.preventDefault();
   let saved = JSON.parse(localStorage.getItem("recipies")) || [];
@@ -123,3 +74,238 @@ function saveRecipe(event) {
 }
 
 $(document).delegate("#saveRecipe", "click", saveRecipe);
+
+/* //Function to initialize the page
+function initializePage() {
+  displaySearchHistory();
+}
+
+//Function to add a search term to the search history
+function addToSearchHistory(searchTerm) {
+  const searchHistory = getSearchHistory();
+  searchHistory.push(searchTerm);
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+  displaySearchHistory();
+}
+
+//Function to get the search history from local storage
+function getSearchHistory() {
+  const searchHistoryJSON = localStorage.getItem('searchHistory');
+  return searchHistoryJSON ? JSON.parse(searchHistoryJSON) : [];
+}
+
+//Function to display the search history
+function displaySearchHistory() {
+  const searchHistory = getSearchHistory();
+  const searchHistoryDiv = document.getElementById('search-history');
+  searchHistoryDiv.innerHTML = '';
+
+  //Add heading for the search history
+  const heading = document.createElement('h4');
+  heading.textContent = 'Search History:';
+  searchHistoryDiv.appendChild(heading);
+
+  //Iterate through the search history and display each item
+  searchHistory.forEach((searchTerm, index) => {
+      const historyItem = document.createElement('div');
+      historyItem.className = 'history-item';
+      historyItem.textContent = searchTerm;
+      historyItem.addEventListener('click', () => {
+          document.getElementById('search-input').value = searchTerm;
+          searchIngredients();
+      });
+      searchHistoryDiv.appendChild(historyItem);
+  });
+}
+
+//Function to clear the search history
+function clearSearchHistory() {
+  localStorage.removeItem('searchHistory');
+  displaySearchHistory();  //Refresh the displayed search history
+}
+
+//Add event listener to the Clear History button
+const clearHistoryButton = document.getElementById('clear-history-button');
+clearHistoryButton.addEventListener('click', clearSearchHistory);
+ */
+
+//Function to be called when "Search Ingredients" button is clicked
+function searchIngredients() {
+  const searchTerm = document.getElementById("search-input").value;
+  if (!searchTerm) {
+    displayModal("Please enter an ingredient.");
+    return;
+  }
+
+  //Fetch meals based on the ingredient
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTerm}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.meals && data.meals.length > 0) {
+        displayMealsWithIngredients(data.meals);
+        /* addToSearchHistory(searchTerm);
+        displaySearchHistory(); //Display search history after adding a new term */
+      } else {
+        displayModal("No meals found with the provided ingredient.");
+      }
+    })
+    .catch(function (error) {
+      console.error("Error fetching data:", error);
+    });
+}
+
+//Function to display ingredients for a meal
+function displayIngredients(meal) {
+  const ingredientsDiv = document.getElementById("ingredient-display");
+  ingredientsDiv.innerHTML = ""; //Clear previous content
+
+  const ingredientsList = document.createElement("ul");
+
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = meal["strIngredient" + i];
+    const measure = meal["strMeasure" + i];
+    if (ingredient) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${ingredient}: ${measure}`;
+      ingredientsList.appendChild(listItem);
+    }
+  }
+
+  ingredientsDiv.appendChild(ingredientsList);
+}
+
+//Function to display a modal with a message
+function displayModal(message) {
+  const modal = document.getElementById("modal");
+  const modalMessage = document.getElementById("modal-message");
+
+  //Set the message content
+  modalMessage.textContent = message;
+
+  //Display the modal
+  modal.style.display = "block";
+
+  //Close the modal when the user clicks on the "x" button
+  const closeBtn = document.querySelector(".close");
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  //Close the modal when the user clicks anywhere outside of the modal
+  window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+}
+
+//Function to display meals with ingredients
+function displayMealsWithIngredients(meals) {
+  const ingredientsDisplay = document.getElementById("ingredient-display");
+  ingredientsDisplay.innerHTML = ""; // Clear previous content
+
+  //Iterate through meals and display meal details
+  meals.forEach((meal) => {
+    const mealDiv = document.createElement("div");
+    mealDiv.className = "meal-item";
+
+    const mealImage = document.createElement("img");
+    mealImage.src = meal.strMealThumb; //Add the meal image source
+    mealImage.alt = meal.strMeal; //Add alt text for accessibility
+    mealImage.className = "meal-image";
+    mealDiv.appendChild(mealImage);
+
+    const mealName = document.createElement("h3");
+    mealName.textContent = meal.strMeal;
+
+    const ingredientsList = document.createElement("ul");
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = meal["strIngredient" + i];
+      const measure = meal["strMeasure" + i];
+      if (ingredient && measure) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${ingredient}: ${measure}`;
+        ingredientsList.appendChild(listItem);
+      }
+    }
+
+    const recipeButton = document.createElement("button");
+    recipeButton.textContent = "View Recipe";
+    recipeButton.addEventListener("click", () => {
+      window.location = "recipe.html?mealId=" + meal.idMeal;
+    });
+
+    mealDiv.appendChild(mealName);
+    mealDiv.appendChild(ingredientsList);
+    mealDiv.appendChild(recipeButton);
+    ingredientsDisplay.appendChild(mealDiv);
+  });
+}
+
+// Function to display a recipe
+function displayRecipe(mealId) {
+  const recipeDisplay = document.getElementById("recipe-display");
+  recipeDisplay.innerHTML = ""; // Clear previous content
+
+  if (!mealId) {
+    return; // Do nothing if there's no mealId
+  }
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.meals && data.meals.length > 0) {
+        const meal = data.meals[0];
+        const recipeDiv = document.createElement("div");
+        recipeDiv.className = "recipe-item";
+
+        const recipeName = document.createElement("h3");
+        recipeName.textContent = meal.strMeal;
+
+        // Display ingredients
+        const ingredientsHeader = document.createElement("h4");
+        ingredientsHeader.textContent = "Ingredients:";
+        recipeDiv.appendChild(ingredientsHeader);
+
+        const ingredientsList = document.createElement("ul");
+        for (let i = 1; i <= 20; i++) {
+          const ingredient = meal["strIngredient" + i];
+          const measure = meal["strMeasure" + i];
+          if (ingredient && measure) {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${ingredient}: ${measure}`;
+            ingredientsList.appendChild(listItem);
+          }
+        }
+        recipeDiv.appendChild(ingredientsList);
+
+        // Display instructions
+        const instructionsHeader = document.createElement("h4");
+        instructionsHeader.textContent = "Instructions:";
+        recipeDiv.appendChild(instructionsHeader);
+
+        const recipeInstructions = document.createElement("p");
+        recipeInstructions.textContent = meal.strInstructions;
+
+        recipeDiv.appendChild(recipeName);
+        recipeDiv.appendChild(recipeInstructions);
+
+        // Add a "Close" button
+        const closeButton = document.createElement("button");
+        closeButton.textContent = "Close";
+        closeButton.addEventListener("click", () => {
+          window.location = "index.html";
+        });
+        recipeDiv.appendChild(closeButton);
+
+        recipeDisplay.appendChild(recipeDiv);
+      } else {
+        alert("Recipe not found.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching recipe:", error);
+    });
+}
